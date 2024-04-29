8853daf544a112521b09fcffe575f0daf91eeead
Yukumo <121703617+IkutaYukumo@users.noreply.github.com>
2024-04-29 16:09:17 +0900

    Add files via upload

--- map.js
+++ map.js
@@ -1,16 +1,30 @@
-(()=>{
+document.addEventListener("DOMContentLoaded", function() {
+    // 全ての.floorLink要素を取得
+    var floorLinks = document.querySelectorAll(".floorLink");
 
-    const button1 = document.getElementsByClassName('floorButton1');
+    // 全ての.floor要素を取得
+    var floorMaps = document.querySelectorAll(".floor");
 
-    const map = document.querySelectorAll('floorMap');
-    map.style.display = 'none';
+    // 各.floorLink要素にクリックイベントリスナーを追加
+    floorLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            // デフォルトのイベントを無効にする
+            event.preventDefault();
 
-    const clickButton =(e)=>{
-        e.preventDefault();
-        map.style.display = 'none';
-        map[1].style.display = 'block';
-    }
+            // クリックされたリンクのdata-target属性の値を取得
+            var targetFloor = this.getAttribute("data-target");
 
-    button1.addEventListener('click',(e)=>clickButton(e));
+            // 全ての.floorMap要素のクラスから"none"を除去
+            floorMaps.forEach(function(map) {
+                map.classList.remove("none");
+            });
 
-})();
+            // 対応する.floorMap要素を表示するために他の要素から"none"を追加
+            floorMaps.forEach(function(map) {
+                if (!map.classList.contains(targetFloor)) {
+                    map.classList.add("none");
+                }
+            });
+        });
+    });
+});
